8f82de3960348d37c3ea61e824545aeae1a87b37
Samuell02 <102335323+Samuell02@users.noreply.github.com>
2026-06-02 14:17:56 -0300

    ss
    sss

--- pds/app/actions/parking.ts
+++ pds/app/actions/parking.ts
@@ -124,7 +124,7 @@ export async function createCheckoutSession(
 
   const appUrl = process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000'
 
-  const baseParams: Stripe.Checkout.SessionCreateParams = {
+  const baseParams: Stripe.Checkout.Session.CreateParams = {
     line_items: [
       {
         price_data: {
